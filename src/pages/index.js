2496986ff6eeb1e50b4ec4a46579df1e080e10b1
Enzo Morvan <enzo.l.morvan@gmail.com>
2021-07-20 13:32:12 +0100

    Add simple post view (WIP)

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Seo from "../components/seo"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
+  const posts = data.allMongodbDesignteamPosts.edges
 
   if (posts.length === 0) {
     return (
@@ -24,10 +24,10 @@ const BlogIndex = ({ data, location }) => {
       <Seo />
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
+          const title = post.node.title
 
           return (
-            <li key={post.fields.slug}>
+            <li key={post.node.link}>
               <article
                 className="post-list-item"
                 itemScope
@@ -35,19 +35,14 @@ const BlogIndex = ({ data, location }) => {
               >
                 <header>
                   <h2>
-                    <Link to={post.fields.slug} itemProp="url">
+                    <Link to={post.node.link} itemProp="url">
                       <span itemProp="headline">{title}</span>
                     </Link>
                   </h2>
-                  <small>{post.frontmatter.date}</small>
+                  <small>{post.node.date}</small>
                 </header>
                 <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
+                  <p>{post.node.description}</p>
                 </section>
               </article>
             </li>
@@ -67,16 +62,14 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      nodes {
-        excerpt
-        fields {
-          slug
-        }
-        frontmatter {
-          date(formatString: "MMMM DD, YYYY")
+    allMongodbDesignteamPosts {
+      edges {
+        node {
           title
+          link
           description
+          date
+          image
         }
       }
     }
